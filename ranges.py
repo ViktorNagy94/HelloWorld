a23d97b7019d24b4135ea6ac218942c16dcdc887
Nagy Viktor <nagy.viktor077@gmail.com>
2021-01-25 08:55:29 +0100

    Ranges with for done. 2 coding exercises done. isuppercase file contains my own solution. It is also correct with a different if (in ABCDEFGHIJKLMNOPQRSTUVWXYZ)

---
+++ ranges.py
@@ -0,0 +1,7 @@
+for i in range (10, 16):
+    print("i is now: {}".format(i))
+
+#Tehát a for a pythonban range-el (is) működik. Amikor egy bizonyos számsor
+#alapján szeretnék végigiterálni egy tömböt akkor for i in range (0,20)
+# kifejezéssel tudok a 0-19 elem között iterálni
+#a kezdőérték beleszámít a végző nem. kezdőérték nélkül 0-tól indul.
--- steppingthroughforloop.py
+++ steppingthroughforloop.py
@@ -1,10 +1,12 @@
-number = "9,223;372:036 854,775;807"
+number = input("Please enter a series of numbers using any separators you like: ")
 separators = ""
 for char in number:
     if not char.isnumeric(): #Ha a char változó értéke nem szám akkor lépünk bele ebbe az ifbe!
         separators = separators + char
-print(separators)
+#print(separators)
 
+values = "".join(char if char not in separators else " " for char in number).split()
+print(sum([int(val) for val in values]))
 
 #values = "".join(char if char not in separators else " " for char in number).split()
 #print([int(val) for val in values])
